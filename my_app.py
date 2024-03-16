096688c851247c3e3cca45acd4f4f9c85ae69b52
Yaffy1311 <162794736+Yaffy1311@users.noreply.github.com>
2024-03-16 12:53:16 +0500

    Add files via upload
    импортировал кое какие детали

--- my_app.py
+++ my_app.py
@@ -1,1 +1,6 @@
-# напиши здесь код основного приложения и первого экрана
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QApplication, QWidget,
+        QHBoxLayout, QVBoxLayout,
+        QGGroupBox, QRadioButton,
+        QPushButton, QLabel, QListWidget, QLineEdit)
